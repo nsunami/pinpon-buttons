a265dcf13f1862668cb3b7bf81ba1b607e371aed
JM Carillo <jazellemaira@pm.me>
2025-07-27 19:24:16 +0200

    feat: Add complete sound and button with associated styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
+
   let rightSound: HTMLAudioElement | null = null
   let wrongSound: HTMLAudioElement | null = null
+  let completeSound: HTMLAudioElement | null = null
 
   const playRightSound = () => {
     if (rightSound) return rightSound.play()
   }
   const playWrongSound = () => {
-    if (wrongSound) return wrongSound.play()
+  const playCompleteSound = () => {
+    completeSound?.play()
   }
 </script>
 
 <main>
   <audio bind:this={rightSound} src="right.mp3" preload="auto"></audio>
   <audio bind:this={wrongSound} src="wrong.mp3" preload="auto"></audio>
+  <audio bind:this={completeSound} src="complete.mp3" preload="auto"></audio>
 
   <div
     class="background flex h-svh flex-col items-center justify-center gap-10 bg-slate-800"
@@ -28,6 +32,12 @@
       class="button wrong"
       on:click={playWrongSound}
       aria-label="A wrong meerkat button"
+    >
+    </button>
+  <button
+      class="button complete"
+      on:click={playCompleteSound}
+      aria-label="A complete sparkles button"
     >
     </button>
   </div>
